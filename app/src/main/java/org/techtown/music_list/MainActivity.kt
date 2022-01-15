babe4d1cfd3c0cff6510c6f625364916108fc12f
임성우 <seongwoorang@naver.com>
2022-01-15 16:47:23 +0900

    navigation
    add nav

--- app/src/main/java/org/techtown/music_list/MainActivity.kt
+++ app/src/main/java/org/techtown/music_list/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //1. navigation 만들기
+
+        //2. Fragment 만들고 연결하기 네비게이션에
+
+        //3. 리사이클러뷰 만들기
     }
 }
